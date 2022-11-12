d0c5b9cc2aeaa2f450989e9898da5440867eb538
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-11-12 14:05:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,37 +7,37 @@ import datetime
 from PIL import Image
 import corpora
 #Page Config
-st.set_page_config(page_title="ITSA")
-st.markdown("# Interactive Text Sentiment Analysis System")
+st.set_page_config(page_title="SIAST")
+st.markdown("# Sistema interactivo de análisis de sentimiento de textos")
 st.markdown("")
-st.sidebar.markdown("# Navigation Pane")
-show_page = st.sidebar.selectbox(label="Choose page", options=["Welcome", "Type In Prompt", "Scrape From Twitter"])
+st.sidebar.markdown("# Menú")
+show_page = st.sidebar.selectbox(label="Elija la página", options=["Welcome", "Type In Prompt", "Scrape From Twitter"])
 
 
 
 if show_page=="Welcome":
     '''
-        ##### Welcome, this interactive web app is aimed to demonstrate the working of text sentiment analysis
+        ##### Bienvenido. Esta aplicación web interactiva tiene como objetivo demostrar el funcionamiento del análisis de sentimiento de texto.
     '''
     image = Image.open('Photos/photo-1523961131990-5ea7c61b2107.jpeg')
     st.image(image)
     
     '''
         ---
-        ##### To begin:
-        - ###### Open the navigation pane
-        - ###### Chose the method to be used for sentiment analysis [Select the desired page from the drop down]
-        - ###### Play around and view analysis
+        ##### Para empezar:
+        - ###### Vaya al menú de la columna izquierda
+        - ###### Elija el método que se utilizará para el análisis de sentimientos [Seleccione la página deseada en el menú desplegable]
+        - ###### Pruebe los distintos métodos y vea el análisis
         ---
-        ##### Few things to keep in mind
+        ##### Agunas cosas para tener en cuenta
 
-        - ###### Polarity refers to the strength of the emotion 
-            - -1 being strongly negative and
-            - +1 being strongly positive
-        - ###### Subjectivity as the name suggests, refers to how subjective the text is
-            - 0 being completely objective [for eg facts]
-            - 1 being completely subjective 
-        - ###### As with all machine learning systems, it will not be 100% accurate and might have biases
+        - ###### La polaridad se refiere a la fuerza de la emoción 
+            - -1 es fuertemente negativo y
+            - +1 es fuertemente positivo
+        - ###### La subjetividad, como su nombre indica, se refiere al grado de subjetividad del texto
+            - 0 es completamente objetivo
+            - 1 es completamente subjetivo 
+        - ###### Como todos los sistemas de aprendizaje automático, no será 100% preciso y podría tener sesgos
         ---
     '''
 
@@ -46,11 +46,11 @@ elif show_page=="Type In Prompt":
     image = Image.open("Photos/photo-1529236183275-4fdcf2bc987e.jpeg")
     st.image(image)
 
-    st.markdown("### Evaluate Your Custom Text")
+    st.markdown("### Evalúe su texto personalizado")
 
-    txt = st.text_area(label="Enter text")
-    corrspell = st.checkbox(label="Use spelling corrector")
-    evaluate = st.button(label="Compute Sentiment")
+    txt = st.text_area(label="Introduzca el texto")
+    corrspell = st.checkbox(label="Use corrector ortográfico (en inglés)")
+    evaluate = st.button(label="Calcule el sentimiento")
 
     #main
     if evaluate and txt!="":
@@ -61,11 +61,11 @@ elif show_page=="Type In Prompt":
             col1, col2 = st.columns([1, 1])
 
             with col1:
-                st.write("Polarity : ")
+                st.write("Polaridad: ")
                 st.write(po)
                 
             with col2:
-                st.write("Subjectivity")
+                st.write("Subjetividad")
                 st.write(su)
             
             chart(po, su)
@@ -75,32 +75,32 @@ else:
     image = Image.open('Photos/sdfsf.png')
     st.image(image)
 
-    st.markdown("### Evaluate Twitter Fetched Text")
-    txt = st.text_area(label="Enter what you want to search")
-    tweetCount = st.slider(label="Choose no of tweets to scrape", min_value=1, value=10, max_value=1150)
+    st.markdown("### Evalúe el texto obtenido en Twitter")
+    txt = st.text_area(label="Introduzca lo que desea buscar")
+    tweetCount = st.slider(label="Elija el número de tweets que se van a raspar", min_value=1, value=10, max_value=1150)
     
     
     #Date Picker
     col1, col2 = st.columns([1, 1])
     with col1:
-        begin=st.date_input(label="Starting Date", value=datetime.date.today()-datetime.timedelta(days=1), min_value=datetime.date.today()-datetime.timedelta(days=1000), max_value=datetime.date.today()-datetime.timedelta(days=1))
+        begin=st.date_input(label="Fecha inicial", value=datetime.date.today()-datetime.timedelta(days=1), min_value=datetime.date.today()-datetime.timedelta(days=1000), max_value=datetime.date.today()-datetime.timedelta(days=1))
     
     with col2:
-        finish=st.date_input(label="Finishing Date", value=datetime.date.today(), min_value=datetime.date.today()-datetime.timedelta(days=999), max_value=datetime.date.today())
+        finish=st.date_input(label="Fecha final", value=datetime.date.today(), min_value=datetime.date.today()-datetime.timedelta(days=999), max_value=datetime.date.today())
     
     #Ensure starting date before finishing date
     if finish<=begin:
-        st.error("Error \nStarting date must be < Finishing Date") 
+        st.error("Error \nLa fecha de inicio debe ser < Fecha de finalización") 
         st.stop()
-    get_tweet = st.button(label="Scrape and Analyze tweets")
+    get_tweet = st.button(label="Raspe y analice los tweets")
     
     if get_tweet:
 
         if txt=="":
-            st.error("Error \nEmpty search string")
+            st.error("Error \nCadena de búsqueda vacía")
             st.stop()
     
-        with st.spinner("Loading"):
+        with st.spinner("Cargando"):
             
 
             data, data_clean = scrape_tweets(txt, tweetCount, begin, finish)
@@ -113,17 +113,17 @@ else:
             st.markdown("")
             st.markdown("")
 
-            st.markdown("## Result")
+            st.markdown("## Resultado")
             st.markdown("---")
             
             tweetSentiment(data_clean)
 
-            with st.expander("Show scraped Twitter tweets"):
+            with st.expander("Mostrar los tweets de Twitter raspados"):
                                 
-                st.markdown("#### Raw")
+                st.markdown("#### Crudo")
                 st.write(data)
                 
-                st.markdown("#### Processed")
+                st.markdown("#### Procesado")
                 st.write(data_clean)
 
 
