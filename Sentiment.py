07caf61c17a42ee08efbf1bdf030ed59ecbbf464
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-11-13 00:55:03 -0600

    Update Sentiment.py

--- Sentiment.py
+++ Sentiment.py
@@ -2,7 +2,7 @@ from numpy import negative, positive
 import streamlit as st
 from textblob import TextBlob
 import pandas as pd
-from textblob.en import polarity, subjectivity
+from textblob.es import polarity, subjectivity
 from chart import *
 
 
