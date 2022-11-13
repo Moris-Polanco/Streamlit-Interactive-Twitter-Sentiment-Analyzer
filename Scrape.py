0ceda633e54748826f64e837bcc0f1bf1e1244d4
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-11-13 01:30:01 -0600

    Update Scrape.py

--- Scrape.py
+++ Scrape.py
@@ -26,7 +26,7 @@ def scrape_tweets(search_query, max_tweets_count=10,start=0, end=0):
         tweets_list3.append([tweet.date, tweet.id, clean(tweet.content), po, su, tweet.user.username])
         
     # Creating a dataframe from the tweets list above
-    tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
-    tweets_df3 = pd.DataFrame(tweets_list3, columns=['Datetime', 'Tweet Id', 'Text', 'Polarity', 'Subjectivity', 'Username'])
+    tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Text', 'Username'])
+    tweets_df3 = pd.DataFrame(tweets_list3, columns=['Datetime', 'Text', 'Polarity', 'Subjectivity', 'Username'])
 
     return tweets_df2, tweets_df3
